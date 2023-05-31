70fecd728afcb85aa3865a17231455f63a7a8249
alanguilhermeM <alanguilherme13@outlook.com>
2023-05-31 14:44:32 -0300

    Finalizado requisito 2

--- src/components/Table.js
+++ src/components/Table.js
@@ -5,6 +5,7 @@ export default function Table() {
   const { fetchApi } = useContext(context);
   const [loading, setLoading] = useState(true);
   const [state, setState] = useState([]);
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
     async function fetchData() {
@@ -15,47 +16,58 @@ export default function Table() {
     fetchData();
   }, []);
 
+  const lowerSearch = search.toLowerCase();
+  const filtredState = state.filter((planet) => planet
+    .name.toLowerCase().includes(lowerSearch));
   return (
     (
       loading ? <h1>Carregando...</h1> : (
-        <table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Rotation Period</th>
-              <th>Orbital Period</th>
-              <th>Diameter</th>
-              <th>Climate</th>
-              <th>Gravity</th>
-              <th>Terrain</th>
-              <th>Surface Water</th>
-              <th>Population</th>
-              <th>Films</th>
-              <th>Created</th>
-              <th>Edited</th>
-              <th>Url</th>
-            </tr>
-          </thead>
-          <tbody>
-            {state.map((planet) => (
-              <tr key={ planet.name }>
-                <td>{planet.name}</td>
-                <td>{planet.rotation_period}</td>
-                <td>{planet.orbital_period}</td>
-                <td>{planet.diameter}</td>
-                <td>{planet.climate}</td>
-                <td>{planet.gravity}</td>
-                <td>{planet.terrain}</td>
-                <td>{planet.surface_water}</td>
-                <td>{planet.population}</td>
-                <td>{planet.films}</td>
-                <td>{planet.created}</td>
-                <td>{planet.edited}</td>
-                <td>{planet.url}</td>
+        <div>
+          <input
+            data-testid="name-filter"
+            value={ search }
+            onChange={ ({ target }) => setSearch(target.value) }
+            placeholder="Busque pelo Planeta"
+          />
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Rotation Period</th>
+                <th>Orbital Period</th>
+                <th>Diameter</th>
+                <th>Climate</th>
+                <th>Gravity</th>
+                <th>Terrain</th>
+                <th>Surface Water</th>
+                <th>Population</th>
+                <th>Films</th>
+                <th>Created</th>
+                <th>Edited</th>
+                <th>Url</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {filtredState.map((planet) => (
+                <tr key={ planet.name }>
+                  <td>{planet.name}</td>
+                  <td>{planet.rotation_period}</td>
+                  <td>{planet.orbital_period}</td>
+                  <td>{planet.diameter}</td>
+                  <td>{planet.climate}</td>
+                  <td>{planet.gravity}</td>
+                  <td>{planet.terrain}</td>
+                  <td>{planet.surface_water}</td>
+                  <td>{planet.population}</td>
+                  <td>{planet.films}</td>
+                  <td>{planet.created}</td>
+                  <td>{planet.edited}</td>
+                  <td>{planet.url}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       )
     )
   );
